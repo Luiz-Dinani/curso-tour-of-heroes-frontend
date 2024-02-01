d1a8a0e36201e3d2aba3ce97bb494bc466727929
Luiz-Dinani <79462872+Luiz-Dinani@users.noreply.github.com>
2024-01-31 23:18:28 -0300

    Usando ngFor, ngIf e (click)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero-model';
+import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,7 +9,14 @@ import { Hero } from '../hero-model';
 })
 export class HeroesComponent {
   hero: Hero = {
-    id: 1,
-    name: "Wolverine"
-  };
+    id: 10,
+    name: 'Teste'
+  }
+  
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect (hero: Hero): void{
+    this.selectedHero = hero;
+  }
 }
---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from "../hero-model";
+
+
+export const HEROES: Hero[] = [
+    {id: 1, name: "Wolverine"},
+    {id: 2, name: "Superman"},
+    {id: 3, name: "Spider Man"},
+    {id: 4, name: "Super Shock"},
+    {id: 5, name: "Capitão Pátria"}
+]
